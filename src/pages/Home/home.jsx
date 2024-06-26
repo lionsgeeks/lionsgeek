076f0c140becb6cfa12882e95a8cd7eff4b0123b
ie-orphane <ilyasselyatime0@gmail.com>
2024-06-26 14:29:54 +0100

    readd colors

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -3,11 +3,26 @@ import { FirstSection } from "./components/firstSection";
 export const HomePage = () => {
   return (
     <>
-      <div className="text-9xl font-bold text-center selection:text-white leading-none">
-        <h1 className="text-banana selection:bg-banana">Lionsgeek is freee</h1>
-        <h1 className="text-apple selection:bg-apple">Lionsgeek is freee</h1>
-        <h1 className="text-grape selection:bg-grape">Lionsgeek is freee</h1>
-        <h1 className="text-avocado selection:bg-avocado">Lionsgeek is freee</h1>
+      <div className="h-[calc(100vh-64px)] text-center text-balance py-12 flex flex-col justify-between gap-14">
+        <div className="flex flex-col gap-6 items-center">
+          <h1 className="text-7xl text-beta font-bold">
+            Unlocking your potential in the digital world.
+          </h1>
+          <p className="text-2xl font-normal">
+            Free digital skills training and mentorship to help you thrive in the tech and media
+            industries.
+          </p>
+
+          <button className="bg-alpha px-16 py-2 w-fit rounded-lg font-medium text-beta">
+            Get Started
+          </button>
+        </div>
+
+        <div className="flex gap-12 px-12 justify-center h-full">
+          <div className="w-[31%] border rounded-lg border-beta h-full"></div>
+          <div className="w-[31%] border rounded-lg border-beta h-full"></div>
+          <div className="w-[31%] border rounded-lg border-beta h-full"></div>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        "apple": "#ff7376",
-        "banana": "#fee819",
-        "avocado": "#51b04f",
-        "grape": "#18BDE8",
-      }
+        beta: "#212529",
+        error: "#ff7376",
+        alpha: "#fee819",
+        good: "#51b04f",
+      },
     },
   },
   plugins: [],
-}
+};
