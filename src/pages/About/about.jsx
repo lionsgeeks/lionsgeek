de1c6553c7a69c82b504d4e06b53ec0cfceb02e3
FireGamerXD <amineg715@gmail.com>
2024-07-01 12:40:08 +0100

    About Page

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,9 +1,17 @@
 import {FirstSectionAbout } from './components/firstSection';
+import { Partners } from './components/partners';
+import Pillers from './components/pillers';
+import { Press } from './components/press';
+import { Stats } from './components/stats';
 
 export const AboutPage = () => {
     return (
         <>
             <FirstSectionAbout/>
+            <Pillers/>
+            <Stats/>
+            <Press/>
+            <Partners/>
         </>
     );
 }
