6e3efe799658296ca634ac3c221939f4db5ab89a
forkanimahdi <forkanimahdi@gmail.com>
2024-11-22 13:05:42 +0100

    make events invisible when there  is  no  backend data

--- src/pages/Events/events.jsx
+++ src/pages/Events/events.jsx
@@ -1,12 +1,17 @@
 // import { FirstSectionEvent } from "./components/firstSection";
+import { useAppContext } from "../../utils/contextProvider";
 import { CardsSection } from "./components/cardsSection";
 import { FirstSectionEvent } from "./components/firstSection";
 
 export const EventPage = () => {
+    const { events } = useAppContext()
     return (
         <>
-            <FirstSectionEvent/>
-            <CardsSection/>
+            <FirstSectionEvent />
+            {
+                events &&
+                <CardsSection />
+            }
         </>
     );
 }
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -33,7 +33,7 @@ export const HomePage = () => {
             </h1>
           </div>
           <div className="flex w-full md:px-48 gap-x-7 md:gap-x-16 gap-5 md:gap-y-14 justify-center flex-wrap">
-            {Array.from({ length: 7 }).map((_, index) => (
+            {Array.from({ length: 6 }).map((_, index) => (
               <img
                 className={`h-12 w-[calc(calc(100%-calc(3*1.75rem))/4)] md:w-[calc(calc(100%-calc(6*4rem))/7)] object-contain`}
                 key={index}
