60f030e2abe5e568add47851c52b580ea0f0cd69
Abdellah-Majdoul <abdellahmajdoul57@gmail.com>
2024-12-18 16:37:19 +0100

    edit dark mode  navbar

--- src/layouts/navbar.jsx
+++ src/layouts/navbar.jsx
@@ -10,7 +10,6 @@ import { LANGUAGES } from "../languages";
 import { useTranslation } from "react-i18next";
 import { MyContext, useAppContext } from "../utils/contextProvider";
 import { HiOutlineLanguage } from "react-icons/hi2";
-import { FaSun , FaMoon } from "react-icons/fa";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [formationMenu, setFormationMenu] = useState(false);
@@ -236,7 +235,7 @@ const Navbar = () => {
                       selectedLanguage == "ar" ? "right-0" : "left-0"
                     }  mt-2 origin-top-right`}
                   >
-                    <div className={`py-2 bg-white rounded-md lg:min-w-[15vw] min-w-[40vw] shadow-lg dark-mode:bg-gray-700 flex flex-col gap-2 ${darkMode && "bg-[#0f0f0f] "}`}>
+                    <div className={`py-2  rounded-md lg:min-w-[15vw] min-w-[40vw] shadow-lg dark-mode:bg-gray-700 flex flex-col gap-2 ${darkMode ? "bg-[#0f0f0f] " : "bg-white"}`}>
                       <Link
                         id="codingLink"
                         to={"/coding"}
@@ -403,24 +402,25 @@ const Navbar = () => {
                         (element) => element.code == selectedLanguage
                       )?.code
                     }
-                  </p>
-                 <svg
-  fill={darkMode ? "white" : "#0f0f0f"} 
-  viewBox="0 0 20 20"
-  className={`inline w-4 h-4 ${
-    selectedLanguage === "ar" ? "mr-1" : "ml-1"
-  } transition-transform duration-75 transform ${
-    languageIsOpen ? "rotate-180" : "rotate-0"
-  }`}
->
-  <path
-    fillRule="evenodd"
-    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-    clipRule="evenodd"
-  />
-</svg>
+                    <svg
+                      fill={darkMode ? "white" : "#0f0f0f"} 
+                      viewBox="0 0 20 20"
+                      className={`inline w-4 h-4 ${
+                        selectedLanguage === "ar" ? "mr-1" : "ml-1"
+                      } transition-transform duration-75 transform ${
+                        languageIsOpen ? "rotate-180" : "rotate-0"
+                      }`}
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>        
+              </p>
+     
 
-                </div>
+              </div>
                 {languageIsOpen && (
                   <div className={`absolute top-10 z-30 mt-2 `}>
                     <div className={`rounded-md  flex flex-col items-start gap-2 shadow-lg  py-2 ${darkMode ? "bg-[#0f0f0f]": "bg-white"}`}>
@@ -440,37 +440,37 @@ const Navbar = () => {
                   </div>
                 )}
               </div>
-              <Button
-                onClick={() => {
-                  navigateTo("contact-us");
-                }}
-                className={"shadow-md font-normal px-4 text-[0.8rem] mt-0"}
-              >
-                {t("header.contact_us")}
-              </Button>
-<button
-  onClick={toggleDarkMode}
-  className="p-2 transition-colors duration-200 rounded-full focus:outline-none"
->
-{darkMode ? (
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="16"
-  height="16"
-  fill={darkMode ? "white" : "currentColor"} // Conditional fill based on darkMode
-  className="bi bi-brightness-high text-yellow-400"
-  viewBox="0 0 16 16"
->
-  <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
-</svg>
+                <Button
+                  onClick={() => {
+                    navigateTo("contact-us");
+                  }}
+                  className={"shadow-md font-normal px-4 text-[0.8rem] mt-0"}
+                >
+                  {t("header.contact_us")}
+                </Button>
+                <button
+                  onClick={toggleDarkMode}
+                  className="p-2 transition-colors duration-200 rounded-full focus:outline-none"
+                >
+                {darkMode ? (
+                  <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill={darkMode ? "white" : "currentColor"} // Conditional fill based on darkMode
+                  className="bi bi-brightness-high text-yellow-400"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
+                </svg>
 
-) : (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon" viewBox="0 0 16 16">
-  <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286"/>
-</svg>
-)}
+                ) : (
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon" viewBox="0 0 16 16">
+                  <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286"/>
+                </svg>
+                )}
 
-</button>
+                </button>
             </nav>
           </div>
         </div>
