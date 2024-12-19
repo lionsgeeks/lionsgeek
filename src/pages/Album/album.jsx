8406dc87b3249f0fc08ad43259a0390c1a58243d
Nassimakhandari <nassomanassima37@gmail.com>
2024-12-19 13:18:50 +0100

    add darkmode of gallery

--- src/pages/Album/album.jsx
+++ src/pages/Album/album.jsx
@@ -7,7 +7,7 @@ export const AlbumPage = () => {
 
     const { id } = useParams();
 
-    const { galleries, selectedLanguage, setSelectedLanguage ,URL, IMAGEURL} = useContext(MyContext);
+    const { galleries, selectedLanguage, setSelectedLanguage ,URL, IMAGEURL , darkMode} = useContext(MyContext);
 
     const [gallery, setGallery] = useState();
 
@@ -16,11 +16,12 @@ export const AlbumPage = () => {
     }, [galleries])
 
     return (
-        <div className='w-full p-10 lg:px-1 py-26 pt-32 '>
-            <h1 className='text-center text-5xl font-bold pb-6'>
+        <div className='w-full p-10 lg:px-1 py-26 pt-32 ' style={{ backgroundColor: darkMode ? "#0f0f0f" : "#ffffff" }}>
+            <h1 className='text-center text-5xl font-bold pb-6' style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}
+            >
                 <TransText {...gallery?.title} />
             </h1>
-            <div className='w-full flex flex-wrap gap-4 justify-center py-10'>
+            <div className='w-full flex flex-wrap gap-4 justify-center py-10' >
                 {
                     gallery?.images.map((image, index) => (
                         <div key={index} className='w-full md:w-[30%] lg:w-[22%] h-60 lg:h-80  rounded-xl'>
