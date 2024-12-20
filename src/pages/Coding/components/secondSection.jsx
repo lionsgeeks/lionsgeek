7b7cb513d7c7c497232286566e14439b9e687bcb
Nassimakhandari <nassomanassima37@gmail.com>
2024-12-20 15:28:13 +0100

    update respo of formation

--- src/pages/Coding/components/secondSection.jsx
+++ src/pages/Coding/components/secondSection.jsx
@@ -61,30 +61,29 @@ export const SecondSection = () => {
       [
         <p
           key={"Front-End0"}
-          className={`flex gap-2 ${selectedLanguage == "ar" ? "flex-row-reverse" : ""
+          className={`flex flex-col lg:flex-row gap-2 ${selectedLanguage == "ar" ? "flex-row-reverse" : ""
             }`}
         >
-          <span className="font-bold text-nowrap " style={{ color: darkMode ? "#ffffff" : "#0f0f0f" } }
+          <span className="font-bold text-nowrap " style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}
           >{`${selectedLanguage == "ar" ? ": HTML " : "HTML : "
             }`}</span>
-          <span style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
+          <span className="lg:pl-0 pl-6" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
             <TransText
               fr="Maîtriser les blocs de construction du web. Apprenez à structurer efficacement votre contenu."
-              en="Master the building blocks of
-          the web. Learn to structure your content effectively."
+              en="Master the building blocks of the web. Learn to structure your content effectively."
               ar="إتقان اللبنات الأساسية للويب. تعلم كيفية بناء المحتوى الخاص بك بشكل فعال "
             />
           </span>
         </p>,
         <p
           key={"Front-End1"}
-          className={`flex gap-2  ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
+          className={`flex flex-col lg:flex-row gap-2  ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
             }`}
         >
           <span className="font-bold text-nowrap" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}
           >{`${selectedLanguage == "ar" ? ": CSS " : "CSS : "
             }`}</span>
-          <span style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
+          <span className="lg:pl-0 pl-6" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
 
             <TransText
               fr="Appliquez des styles à vos pages web avec précision. Comprenez les techniques de mise en page, la conception adaptative et les frameworks CSS modernes"
@@ -96,39 +95,39 @@ export const SecondSection = () => {
         </p>,
         <p
           key={"Front-End2"}
-          className={`flex gap-2  ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
+          className={`flex flex-col lg:flex-row gap-2  ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
             }`}
         >
           <span className="font-bold text-nowrap" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}
           >{`${selectedLanguage == "ar" ? ": JavaScript " : "JavaScript : "
             }`} </span>
-                      <span style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
+          <span className="lg:pl-0 pl-6" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
 
-          <TransText
-            fr="Donnez vie à vos pages web grâce à l'interactivité. Apprenez les concepts JavaScript de base et la manipulation du DOM"
-            en="Bring your web pages to life with interactivity. Learn core JavaScript concepts and how to manipulate the DOM."
-            ar="  DOM وكيفية معالجة  JavaScriptأضف الحيوية إلى صفحات الويب الخاصة بك من خلال التفاعلية. تعلم المفاهيم الأساسية لـ "
-          />
-                    </span>
+            <TransText
+              fr="Donnez vie à vos pages web grâce à l'interactivité. Apprenez les concepts JavaScript de base et la manipulation du DOM"
+              en="Bring your web pages to life with interactivity. Learn core JavaScript concepts and how to manipulate the DOM."
+              ar="  DOM وكيفية معالجة  JavaScriptأضف الحيوية إلى صفحات الويب الخاصة بك من خلال التفاعلية. تعلم المفاهيم الأساسية لـ "
+            />
+          </span>
 
         </p>,
         <p
           key={"Front-End3"}
-          className={`flex gap-2  ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
+          className={`flex flex-col lg:flex-row gap-2  ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
             }`}
         >
           <span className="font-bold text-nowrap" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}
           >
             {`${selectedLanguage == "ar" ? ": React " : "React : "}`}{" "}
           </span>
-          <span style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
+          <span className="lg:pl-0 pl-6" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
 
-          <TransText
-            fr="Plongez dans l'une des bibliothèques JavaScript les plus populaires pour la création d'interfaces utilisateur. Apprenez l'architecture basée sur les composants et la gestion d'état."
-            en="Dive into one of the most popular JavaScript libraries for building user interfaces. Learn component-based architecture and state management."
-            ar=" JavaScript شيوعًا لبناء واجهات المستخدم. تعلم بنية المكونات وإدارة الحالة.تعمق في واحدة من أكثر مكتبات"
-          />
-                    </span>
+            <TransText
+              fr="Plongez dans l'une des bibliothèques JavaScript les plus populaires pour la création d'interfaces utilisateur. Apprenez l'architecture basée sur les composants et la gestion d'état."
+              en="Dive into one of the most popular JavaScript libraries for building user interfaces. Learn component-based architecture and state management."
+              ar=" JavaScript شيوعًا لبناء واجهات المستخدم. تعلم بنية المكونات وإدارة الحالة.تعمق في واحدة من أكثر مكتبات"
+            />
+          </span>
 
         </p>,
       ],
@@ -139,7 +138,7 @@ export const SecondSection = () => {
       [
         <p
           key={"Back-End0"}
-          className={`flex gap-2 ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
+          className={`flex flex-col lg:flex-row gap-2 ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
             }`}
         >
           <span className="font-bold  text-nowrap" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}
@@ -147,14 +146,14 @@ export const SecondSection = () => {
             {" "}
             {`${selectedLanguage == "ar" ? ": Laravel " : "Laravel : "}`}{" "}
           </span>
-          <span style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
+          <span className="lg:pl-0 pl-6" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
 
-          <TransText
-            fr="Familiarisez-vous avec ce puissant framework PHP. Apprenez à construire des applications côté serveur robustes et évolutives, à gérer des bases de données et à créer des API RESTful"
-            en="Get hands-on with this powerful PHP framework. Learn to build robust and scalable server-side applications, manage databases, and create RESTful APIs."
-            ar="اكتسب خبرة عملية مع إطار PHP القوي هذا. تعلم كيفية بناء تطبيقات قوية وقابلة للتوسيع على جانب الخادم, وإدارة قواعد البيانات, وإنشاء واجهات برمجة تطبيقات RESTful"
-          />
-                    </span>
+            <TransText
+              fr="Familiarisez-vous avec ce puissant framework PHP. Apprenez à construire des applications côté serveur robustes et évolutives, à gérer des bases de données et à créer des API RESTful"
+              en="Get hands-on with this powerful PHP framework. Learn to build robust and scalable server-side applications, manage databases, and create RESTful APIs."
+              ar="اكتسب خبرة عملية مع إطار PHP القوي هذا. تعلم كيفية بناء تطبيقات قوية وقابلة للتوسيع على جانب الخادم, وإدارة قواعد البيانات, وإنشاء واجهات برمجة تطبيقات RESTful"
+            />
+          </span>
 
         </p>,
       ],
@@ -165,39 +164,39 @@ export const SecondSection = () => {
       [
         <p
           key={"Back-End0"}
-          className={`flex gap-2 ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
+          className={`flex flex-col lg:flex-row gap-2 ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
             }`}
         >
           <span className="font-bold text-nowrap" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}
           >
             {`${selectedLanguage == "ar" ? ": Git " : "Git : "}`}{" "}
           </span>
-          <span style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
+          <span className="lg:pl-0 pl-6" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
 
-          <TransText
-            fr="Comprendre les notions essentielles du contrôle de version. Apprenez à suivre les modifications de votre base de code, à collaborer avec d'autres et à gérer les versions de projet"
-            en="Understand the essentials of version control. Learn how to track changes in your codebase, collaborate with others, and manage project versions."
-            ar="فهم أساسيات مراقبة الإصدارات. تعلم كيفية تتبع التغييرات في قاعدة الكود الخاصة بك, والتعاون مع الآخرين, وإدارة إصدارات المشروع"
-          />
-                    </span>
+            <TransText
+              fr="Comprendre les notions essentielles du contrôle de version. Apprenez à suivre les modifications de votre base de code, à collaborer avec d'autres et à gérer les versions de projet"
+              en="Understand the essentials of version control. Learn how to track changes in your codebase, collaborate with others, and manage project versions."
+              ar="فهم أساسيات مراقبة الإصدارات. تعلم كيفية تتبع التغييرات في قاعدة الكود الخاصة بك, والتعاون مع الآخرين, وإدارة إصدارات المشروع"
+            />
+          </span>
 
         </p>,
         <p
           key={"Back-End1"}
-          className={`flex gap-2  ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
+          className={`flex flex-col lg:flex-row gap-2  ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
             }`}
         >
           <span className="font-bold  text-nowrap" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}
           >{`${selectedLanguage == "ar" ? ": GitHub " : "GitHub : "
             }`}</span>
-                      <span style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
+          <span className="lg:pl-0 pl-6" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
 
-          <TransText
-            fr="Explorez cette plateforme populaire d'hébergement et de partage de code. Apprenez à utiliser GitHub pour le contrôle de version, la collaboration et la gestion de projet"
-            en="Explore this popular platform for hosting and sharing code. Learn how to use GitHub for version control, collaboration, and project management."
-            ar="استكشف هذه المنصة الشهيرة لاستضافة ومشاركة الكود. تعلم كيفية استخدام GitHub لمراقبة الإصدارات, والتعاون, وإدارة المشروع"
-          />
-                    </span>
+            <TransText
+              fr="Explorez cette plateforme populaire d'hébergement et de partage de code. Apprenez à utiliser GitHub pour le contrôle de version, la collaboration et la gestion de projet"
+              en="Explore this popular platform for hosting and sharing code. Learn how to use GitHub for version control, collaboration, and project management."
+              ar="استكشف هذه المنصة الشهيرة لاستضافة ومشاركة الكود. تعلم كيفية استخدام GitHub لمراقبة الإصدارات, والتعاون, وإدارة المشروع"
+            />
+          </span>
 
         </p>,
       ],
@@ -208,7 +207,7 @@ export const SecondSection = () => {
       [
         <p
           key={0}
-          className={`flex gap-2  ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
+          className={`flex flex-col lg:flex-row gap-2  ${selectedLanguage == "ar" ? "flex-row-reverse " : ""
             }`}
         >
           <span className="font-bold text-nowrap" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}
@@ -216,14 +215,14 @@ export const SecondSection = () => {
             ? " : Shell Scripting"
             : "Shell Scripting : "
             }`}</span>
-                      <span style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
+          <span className="lg:pl-0 pl-6" style={{ color: darkMode ? "#ffffff" : "#0f0f0f" }}>
 
-          <TransText
-            fr="Automatisez des tâches et améliorez votre workflow. Apprenez les bases du scripting shell, les outils en ligne de commande et comment écrire des scripts pour exécuter efficacement des tâches répétitives."
-            en="Automate tasks and improve your workflow. Learn the basics of shell scripting, command-line tools, and how to write scripts to perform repetitive tasks efficiently."
-            ar="أتمتة المهام وتحسين سير العمل الخاص بك. تعلم أساسيات كتابة نصوص الأوامر, وأدوات سطر الأوامر, وكيفية كتابة نصوص لتنفيذ المهام المتكررة بكفاءة  "
-          />
-                    </span>
+            <TransText
+              fr="Automatisez des tâches et améliorez votre workflow. Apprenez les bases du scripting shell, les outils en ligne de commande et comment écrire des scripts pour exécuter efficacement des tâches répétitives."
+              en="Automate tasks and improve your workflow. Learn the basics of shell scripting, command-line tools, and how to write scripts to perform repetitive tasks efficiently."
+              ar="أتمتة المهام وتحسين سير العمل الخاص بك. تعلم أساسيات كتابة نصوص الأوامر, وأدوات سطر الأوامر, وكيفية كتابة نصوص لتنفيذ المهام المتكررة بكفاءة  "
+            />
+          </span>
 
         </p>,
       ],
@@ -266,7 +265,7 @@ export const SecondSection = () => {
                   ? "text-right lg:flex-row-reverse"
                   : ""
                   }`}
-                style={{ backgroundColor: darkMode ? "#0f0f0f" : "#ffffff", }}
+                style={{ backgroundColor: darkMode ? "#212529" : "#ffffff", }}
               >
                 <h1
                   className={`flex gap-3 ${selectedLanguage === "ar"
