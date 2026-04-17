585f862e5dc93aeb4f2a89dc9cff0a276522750f
yahya-moussair <yahyamoussair05@gmail.com>
2026-04-17 14:45:52 +0100

    chore: update dependencies and add AI_API_KEY to .env.example

--- resources/js/components/SubStringText.jsx
+++ resources/js/components/SubStringText.jsx
@@ -1,5 +1,5 @@
 const SubstringText = ({ text, length }) => {
-    return <>{text.length > length ? `${text.substring(0, length)}...` : `${text}`}</>;
+    return <>{text?.length > length ? `${text.substring(0, length)}...` : `${text}`}</>;
 };
 
 export default SubstringText;
