126915a781e1cc65cd57a6fc6b6e7f3cf78b4eb7
yahya-moussair <yahyamoussair05@gmail.com>
2026-06-13 15:13:52 +0100

    refactor: remove hardcoded user and info session seeders for cleaner database seeding

--- database/seeders/InfoSessionSeeder.php
+++ database/seeders/InfoSessionSeeder.php
@@ -14,33 +14,7 @@ class InfoSessionSeeder extends Seeder
     public function run(): void
     {
         // Create two upcoming sessions: one Coding, one Media
-        InfoSession::updateOrCreate(
-            [
-                'name' => 'Coding Info Session',
-                'start_date' => Carbon::now()->addDays(7)->toDateTimeString(),
-            ],
-            [
-                'formation' => 'Coding',
-                'isAvailable' => true,
-                'isFull' => false,
-                'isFinish' => false,
-                'places' => 50,
-            ]
-        );
-
-        InfoSession::updateOrCreate(
-            [
-                'name' => 'Media Info Session',
-                'start_date' => Carbon::now()->addDays(10)->toDateTimeString(),
-            ],
-            [
-                'formation' => 'Media',
-                'isAvailable' => true,
-                'isFull' => false,
-                'isFinish' => false,
-                'places' => 40,
-            ]
-        );
+        
     }
 }
 
